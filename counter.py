e4942ee142631b1fa6bfc0da98734b89b7940ed8
M1CH4ELP0 <91473896+M1CH4ELP0@users.noreply.github.com>
2021-12-04 16:32:21 +0300

    Create counter.py

---
+++ counter.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+win = tk.Tk()
+canvas = tk.Canvas(win, bg='white', width = 400, height = 400)
+for i in range(0, 400, 50):
+    line_gor = canvas.create_line((50 + i, 0), (50 + i, 400))
+for i in range(0, 400, 50):
+    line_ver = canvas.create_line((0, 50 + i), (400, 50 + i))
+for i in range(0, 400, 100):
+    oval = canvas.create_oval((0+i, 0), (50+i,50), fill="#EE82EE" )
+    oval = canvas.create_oval((50+i, 50), (100+i,100), fill="#EE82EE" )
+    oval = canvas.create_oval((0+i, 100), (50+i,150), fill="#EE82EE" )
+for i in range(0, 400, 100):
+    oval = canvas.create_oval((50+i, 0), (100+i,50), fill="#EE82EE" )
+    oval = canvas.create_oval((50+i, 50), (100+i,100), fill="#EE82EE" )
+    oval = canvas.create_oval((0+i, 100), (50+i,150), fill="#EE82EE" )
+canvas.pack()
+win.mainloop()
