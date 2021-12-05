654606bd8a8283c75c36a1bc613241893f7313b0
M1CH4ELP0 <91473896+M1CH4ELP0@users.noreply.github.com>
2021-12-05 18:25:05 +0300

    Update counter.py

--- counter.py
+++ counter.py
@@ -11,8 +11,8 @@ for i in range(0, 400, 100):
     oval = canvas.create_oval((50+i, 50), (100+i,100), fill="#EE82EE" )
     oval = canvas.create_oval((0+i, 100), (50+i,150), fill="#EE82EE" )
 for i in range(0, 400, 100):
-    oval = canvas.create_oval((50+i, 0), (100+i,50), fill="#EE82EE" )
-    oval = canvas.create_oval((50+i, 50), (100+i,100), fill="#EE82EE" )
-    oval = canvas.create_oval((0+i, 100), (50+i,150), fill="#EE82EE" )
+    oval = canvas.create_oval((0+i, 250), (50+i,300), fill="#66f58a" )
+    oval = canvas.create_oval((50+i, 300), (100+i,350), fill="#66f58a" )
+    oval = canvas.create_oval((0+i, 350), (50+i,400), fill="#66f58a" )
 canvas.pack()
 win.mainloop()
