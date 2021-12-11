bdb4d02948c4918e6de7e6a5b7718434bf1f7c87
M1CH4ELP0 <91473896+M1CH4ELP0@users.noreply.github.com>
2021-12-11 16:36:05 +0300

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,18 +1,63 @@
-import tkinter as tk
+import tkinter
+import random
+from tkinter import PhotoImage
 
-win = tk.Tk()
-canvas = tk.Canvas(win, bg='white', width = 400, height = 400)
-for i in range(0, 400, 50):
-    line_gor = canvas.create_line((50 + i, 0), (50 + i, 400))
-for i in range(0, 400, 50):
-    line_ver = canvas.create_line((0, 50 + i), (400, 50 + i))
-for i in range(0, 400, 100):
-    oval = canvas.create_oval((0+i, 0), (50+i,50), fill="#EE82EE" )
-    oval = canvas.create_oval((50+i, 50), (100+i,100), fill="#EE82EE" )
-    oval = canvas.create_oval((0+i, 100), (50+i,150), fill="#EE82EE" )
-for i in range(0, 400, 100):
-    oval = canvas.create_oval((0+i, 250), (50+i,300), fill="#66f58a" )
-    oval = canvas.create_oval((50+i, 300), (100+i,350), fill="#66f58a" )
-    oval = canvas.create_oval((0+i, 350), (50+i,400), fill="#66f58a" )
+def prep():
+    global player
+
+    canvas.delete('all')
+    player_pos = (random.randint(1, N_X - 1)*step,
+                    random.randint(1,N_Y -1)*step)
+    player= canvas.create_image(
+        (player_pos[0], player_pos[1]), image=player_pic, anchor='nw')
+    label.config(text='ESCAPE!')
+    master.bind('<KeyPress>', key_pressed)
+
+def move_wrap(obj, move_x, move_y):
+    xy = canvas.coords(obj)
+    canvas.move(obj, move_x, move_y)
+    print(xy)
+    if xy[0] <= 0:
+        canvas.move(obj, WIDTH, 0)
+    if xy[0] >= WIDTH:
+        canvas.move(obj, -WIDTH, 0)
+    if xy[1] <= 0:
+        canvas.move(obj, 0, HEIGHT)
+    if xy[1] >=HEIGHT:
+        canvas.move(obj, 0, -HEIGHT)
+    
+def key_pressed(event):
+    if event.keysym == 'Up':
+        move_wrap(player, 0, -step)
+    elif event.keysym == 'Down':
+        move_wrap(player, 0, step)
+    elif event.keysym == 'Right':
+        move_wrap(player, step, 0)
+    elif event.keysym == 'Left':
+        move_wrap(player, -step, 0)
+
+master = tkinter.Tk()
+
+step = 32
+N_X = 10
+N_Y = 10
+WIDTH = step * N_X
+HEIGHT = step * N_Y
+a = False
+player_pic = tkinter.PhotoImage(file=r'.\pixil-frame-0.png')
+
+canvas= tkinter.Canvas(master, bg = '#d5f05f',
+                        width=WIDTH, height=HEIGHT)
+
+player_pos = (random.randint(0,N_X - 1)* step,
+                random.randint(0,N_Y -1)*step)
+print(player_pos)
+label= tkinter.Label(master, text= 'ЫЫЫЫЫЫ!')
+restart= tkinter.Button(master, text = 'Restart',
+                            command= prep)
+restart.pack()
+label.pack()
 canvas.pack()
-win.mainloop()
+prep()
+master.bind('<KeyPress>', key_pressed)
+master.mainloop()
