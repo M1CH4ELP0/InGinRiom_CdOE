d3e29764a7292fefbb7aab75a00f406441e079e0
M1CH4ELP0 <91473896+M1CH4ELP0@users.noreply.github.com>
2021-12-25 16:33:44 +0300

    Update counter.py

--- counter.py
+++ counter.py
@@ -1,16 +1,43 @@
 import pygame
 pygame.init()
-a = int(input())
-d = int(input())
-x = a / d
-win = pygame.display.set_mode((a, a))
-for f in range(0, a, 2):
-    for i in range(0, a, 2):
-        pygame.draw.rect(win, (225, 225, 225), (int(x*i), f * 50, int(x), int(x)))
-        pygame.draw.rect(win, (225, 225, 225), (int(x*i+50), f * 50 + 50, int(x), int(x)))
-pygame.display.update()
-pygame.display.set_caption('aaaAAAaAAAAAaaa')
+win = pygame.display.set_mode((500, 500))
+
+x = 100
+y = 150
+y1 = 50
+x1 = 250
+dc= 0
+dv = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
+
+    if dc == 1:
+        x=x-1
+    elif dc == 0:
+        x = x+1
+
+    if x > 400:
+        dc += 1
+    if x <0:
+        dc -= 1
+
+    win.fill((225, 225, 225))
+    pygame.draw.rect(win, (225, 225, 0), (x, y, 100, 150))
+
+    if dv == 1:
+        y1=y1-1
+    elif dv == 0:
+        y1 = y1+1
+
+    if y1 > 450:
+        dv+=1
+    elif y1 < 50:
+        dv-=1
+        
+
+    pygame.draw.circle(win, (155, 225, 45), (x1, y1), 50)
+    pygame.display.update()
+
+    pygame.time.delay(5)
