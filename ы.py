7336e3a858d39f26b53f130a2c1fa1a2e820f68c
M1CH4ELP0 <91473896+M1CH4ELP0@users.noreply.github.com>
2022-02-05 16:41:18 +0300

    Update ы.py

--- ы.py
+++ ы.py
@@ -1,40 +1,39 @@
+from msvcrt import LK_LOCK
 import pygame
 pygame.init()
+import random
 win = pygame.display.set_mode((500, 500))
 win.fill((100, 100, 100))
-FPS = 60
+
 clock = pygame.time.Clock()
 class Circle:
-    def __init__(self, x, y, rad, col):
+    def __init__(self, x, y, rad, col, dir):
         self.x = x
         self.y = y
         self.rad = rad
         self.col = col
+        self.dir = dir
     def draw(self, win):
         pygame.draw.circle(win, self.col, (self.x, self.y), self.rad)
     def move(self):
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            self.x-=10
-        elif keys[pygame.K_RIGHT]:
-            self.x += 10
-        elif keys[pygame.K_UP]:
-            self.y-= 10
-        elif keys[pygame.K_DOWN]:
-            self.y+= 10
-    def upd(self):
-        pygame.display.update()
-        win.fill((100, 100, 100))
-a= Circle(250, 250, 50, 'yellow')
-a.draw(win)
-
-
+        if self.dir == 'right':
+            self.x += 1
+            if self.x > 500:
+                self.dir ='left'
+        else:
+            self.x -=1
+            if self.x < 0:
+                self.dir = 'right'
+list_circles = []
+for i in range(0, 500, 1):
+    list_circles.append(Circle(i*10, i*5, 30, random.choices(range(256), k=3), 'right'))
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             exit()
-    a.move()
-    a.draw(win)
-    a.upd()
-    clock.tick(FPS)
+    win.fill((100, 100, 100))
+    for i in range(100):
+        list_circles[i].move()
+        list_circles[i].draw(win)
+    pygame.display.update()
