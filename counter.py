44dde67871f9782dda90c947d85f1d5cd65a56e6
M1CH4ELP0 <91473896+M1CH4ELP0@users.noreply.github.com>
2022-02-19 16:43:41 +0300

    gfrtd
    rt

--- counter.py
+++ counter.py
@@ -3,37 +3,41 @@ import random
 pg.init()
 win = pg.display.set_mode((500, 500))
 win.fill((0, 0, 0))
-class Star:
-    def __init__(self, pressed):
-        self.x = pressed[0]
-        self.y = pressed[1]
-        self.obj = 'rect'
-    def draw(self,win, fig):
-        self.obj = fig
-        if self.obj == 'circle':
-            pg.draw.circle(win, ((random.choices(range(0, 256), k=3))), pg.mouse.get_pos(), 30)
-        elif self.obj == 'rect':
-            pg.draw.rect(win, ((random.choices(range(256), k=3))), (self.x, self.y, 100, 150))
+
+cross = '#046582'
+circle = '#e4bad4'
+class Board:
+    def __init__(self, W, H, size):
+        self.w, self.h = W, H
+        self.size = size
+        self.board = [
+            [0, 0, 0]
+            [0, 0, 0]
+            [0, 0, 0]
+        ]
+        self.move = 1
+    def click(self,mouse_pos):
+        x = mouse_pos[0] // self.size
+        y = mouse_pos[1] // self.size
+        self.board[y][x] = self.move
+        self.move = -self.move
+    def render(self, screen):
+        
+        
+        
     
 
-            
+board = Board(600, 600, 200)
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
-
-    a = Star
-    pressed = pg.mouse.get_pressed()
-    keys = pg.key.get_pressed()
-    if pressed[0]:
-        pos = pg.mouse.get_pos()
-        a = Star(pos)
-        #qqqa.draw(win)
-        if keys[pg.K_w]:
-            a.draw(win,'circle')
-        if keys[pg.K_q]:
-            a.draw(win, 'rect')
-    if keys[pg.K_SPACE]:
-        win.fill((0, 0, 0))
-        
+        if event.type == pg.MOUSEBUTTONDOWN:
+            board.click(event.pos)
+    screen.fill(255, 255, 255)
+    board.render(screen)
     pg.display.update()
+    keys = pg.key.get_pressed()
+    if keys[pg.K_ESCAPE]:
+        pg.quit()
+        exit
--- ы.py
+++ ы.py
@@ -1,39 +1,4 @@
-
-import pygame
-pygame.init()
-import random
-win = pygame.display.set_mode((500, 500))
-win.fill((100, 100, 100))
-
-clock = pygame.time.Clock()
-class Circle:
-    def __init__(self, x, y, rad, col, dir):
-        self.x = x
-        self.y = y
-        self.rad = rad
-        self.col = col
-        self.dir = dir
-    def draw(self, win):
-        pygame.draw.circle(win, self.col, (self.x, self.y), self.rad)
-    def move(self):
-        if self.dir == 'right':
-            self.x += 1
-            if self.x > 500:
-                self.dir ='left'
-        else:
-            self.x -=1
-            if self.x < 0:
-                self.dir = 'right'
-list_circles = []
-for i in range(0, 500, 1):
-    list_circles.append(Circle(i*10, i*5, 30, random.choices(range(256), k=3), 'right'))
-
-while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit()
-    win.fill((100, 100, 100))
-    for i in range(100):
-        list_circles[i].move()
-        list_circles[i].draw(win)
-    pygame.display.update()
+b = int(input())
+c = int(input())
+a = b // c
+print(a)
