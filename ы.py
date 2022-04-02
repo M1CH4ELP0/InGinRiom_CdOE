1be59277240621146473918aaad23b0b60fa95c7
M1CH4ELP0 <91473896+M1CH4ELP0@users.noreply.github.com>
2022-04-02 16:40:10 +0300

    Update ы.py

--- ы.py
+++ ы.py
@@ -1,27 +1,28 @@
 import pygame as pg
+import random
+pg.init()
+W, H, fps = 500, 500, 120
+size = (W, H)
+clock = pg.time.Clock()
 
 pg.init()
-size = W, H = 1920, 1080
-fps = 30
 win = pg.display.set_mode(size)
-def l(name):
-    img = pg.image.load(name)
-    img = img.convert()
-    colorkey = img.get_at((0, 0))
-    img.set_colorkey(colorkey)
-    return img
 
-
-img = pg.image.load('400px-Vexillum_Ucrainae.svg.png')
-img1 = pg.transform.scale(img, (200, 200))
-img2 = pg.transform.scale(img, (700, 200))
-im = pg.image.load('f.jpg')
+class Circle:
+    def __init__(self, x, y, rad):
+    def move(self):
+    def show(self):
+circles = []
+for i in range(100):
+    circles.append(Circle(W // 2, H // 2, 50))
 while True:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             exit()
-
-    win.blit(img1, (500, 0))
-    win.blit(img2, (700, 0))
-    win.blit(im, (200, 200))
+    for circle in circles:
+        circle.move()
+    win.fill(225, 225, 225)
+    for circle in circles:
+        circle.show
     pg.display.update()
+    clock.tick(fps)
