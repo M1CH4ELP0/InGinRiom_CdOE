881a9cffe61c3f9e9f538b8a5e2bea38158ae792
M1CH4ELP0 <91473896+M1CH4ELP0@users.noreply.github.com>
2022-05-27 23:12:30 +0300

    Create HAPPY BIRTHDAY.py
    sdf

---
+++ HAPPY BIRTHDAY.py
@@ -0,0 +1,23 @@
+import pygame as pg
+
+pg.init()
+win = pg.display.set_mode((500, 500))
+win.fill((51, 64, 43))
+W, H = 600, 600
+
+f1 = pg.font.Font(None, 36)
+text1 = f1.render('        С ДНЁМ РОЖДЕНИЯ, КАТЯ!!', 1, (98, 255, 0))
+
+dog_surf = pg.image.load('op.png')
+dog_rect = dog_surf.get_rect(
+    bottomright=(500, 500))
+win.blit(dog_surf, dog_rect)
+
+win.blit(text1, (10, 50))
+while True:
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
+            exit()
+    pg.display.update()
+
